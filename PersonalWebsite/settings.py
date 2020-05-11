e92e623c9c9446273d834b36239ceafa0019855d
ZeelSinojia <sinojia.zeel2@gmail.com>
2020-05-11 17:07:47 +0530

    added environment variables

--- PersonalWebsite/settings.py
+++ PersonalWebsite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's4&+e=+$5dv-iwti_rmltaxt+51f(2s@bie8-bq%o!d6))!m!p'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -126,8 +126,8 @@ STATIC_URL = '/static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'sinojia.zeel2@gmail.com'
-EMAIL_HOST_PASSWORD = 'jpsbzahdulregzdi'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
